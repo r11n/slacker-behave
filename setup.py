7638ef9dac8a377ddfe425a5bcd10fd57f2354cd
raghavendranekkanti <raghavendra.nekkanti@inmar.com>
2019-12-24 11:33:55 +0530

    update setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = 'slackerbehave',
     packages = ['slackerbehave'],
-    version = '0.0.5',
+    version = '0.0.6',
     license = 'MIT',
     description = 'A slack report generator for behave using the behave hooks',
     author = 'raghavendranekkanti',
     author_email = 'raghavendra.nekkanti@inmar.com',
     url = 'https://github.com/raghavendranekkanti/slacker-behave',
-    download_url = 'https://github.com/raghavendranekkanti/slacker-behave/archive/0.0.5.tar.gz',
+    download_url = 'https://github.com/raghavendranekkanti/slacker-behave/archive/0.0.6.tar.gz',
     keywords = ['slack-webhook', 'behave-reports', 'reports', 'behave', 'slack'],
     install_requires = [
         'requests'
